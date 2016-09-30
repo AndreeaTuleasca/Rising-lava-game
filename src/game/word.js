394a64d7f950ad8de4743a5699d17bb0d3ab5c5a
AndreeaTuleasca <andreea.tuleasca@gmail.com>
2016-09-30 13:40:41 +0100

    Created input for word

--- src/game/word.js
+++ src/game/word.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 
 class Word extends Component {
+    constructor(){
+        super();
+        this.word= "blablabla";
+        this.state = {typedWord: "bla"};
+    }
     render(){
         return (
-            <div className="word">this is word placed at: {this.props.position}</div>
+            <div className="word">this is word placed at: {this.props.position}
+                <div>
+                    <input type="text" value={this.state.typedWord} onChange={this.handleChange.bind(this)}/>
+                </div>
+            </div>
         );
+    }
+
+    handleChange(event){
+        // let word = event.target.value
+        // let lastLetterIndex = typedWord.length-1;
+        // let lastLetter = typedWord.charAt(lastLetterIndex);
+        this.setState({typedWord: event.target.value}); 
     }
 }
 

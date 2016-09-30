4bbaff40ce253a4ce0b8e406997abac0ed276ceb
XENOpz <angel_lakov@yahoo.com>
2016-09-30 16:46:33 +0100

    Added a witty message to the distance when the lava gets you.

--- src/gui/distanceDisplay.js
+++ src/gui/distanceDisplay.js
@@ -4,7 +4,7 @@ class DistanceDisplay extends Component {
     render(){
         return (
             <div className="distance-display">
-            Distance: {this.props.distance}
+            Distance: {this.props.distance > 0 ? this.props.distance : "Way too close!"}
             </div>
         );
     }
